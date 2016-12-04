90627b5efaebe71fbc5408232a8d2ac1c8a6ae56
Joao Roberto Lima <jbetol@gmail.com>
2016-12-04 21:12:01 -0200

    Exercise 04 finished

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,33 +1,47 @@
-#write your code here
+
+#translate word
 def traduz(text)
-	if text[0] =~ /[aeiou]/
+	if text[0] =~ /[aeiou]/  #start with vowel
 		text  = text << "ay" 
-	else
-		text = swapFirstLetter(text)
-	end
-	return text
+	else	
+		text = swapFirstLetter(text) #start with consonant
+	end	
 end
 
+#translate a text
 def translate(text)
-	i= text.split
-	i1=traduz(i[0])
-	if i.size > 1
-		i2=" "+traduz(i[1])
-	else
-		i2=" "
-	end
-	return (i1+i2).strip
+	words = text.split
+	word=""
+	for i in 0..words.size-1
+		word+=" "+traduz(words[i])
+	end	
+	return word.strip
 end
 
 def swapFirstLetter(text)
-# Is not a vowel
-	tempLetter = text[0]
-	text[0] = " "
-	if (text[1] =~/[aeiou]/)==nil
-		tempLetter += text[1]
-		text[1] = " "
-#	elsif (text[2] =~/[aeiou]/)==nil)
-	end	
+# Start with consonant
+	tempLetter = ""  #used to move letters to the end of the word
+
+	if text.rindex('squ',2)!=nil  #the word starts with "squ" 
+		text << 'squ'
+		2.downto 0 do |i|
+		text[i] = " "
+		end
+	else # manipulate the first consonant
+		tempLetter = text[0]
+		text[0] = " "
+		if (text[1] =~/[aeiou]/)==nil 
+			tempLetter += text[1] #second consonant
+			text[1] = " "
+			if ((text[2] =~/[aeiou]/)==nil) #third consonant?
+				tempLetter += text[2]
+				text[2] = " "
+			end	
+		elsif text[1]=="u" and tempLetter.downcase=="q" # dealing with "QU" 
+				tempLetter += text[1]
+				text[1] = " "				
+		end	
+	end
 	text << tempLetter+"ay"
 	return text.strip!
 end
