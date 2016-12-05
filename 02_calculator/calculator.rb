7ca7fb457c8857fc1f5941fe3c568ac9d8432074
Joao Roberto Lima <jbetol@gmail.com>
2016-12-05 14:54:11 -0200

    Completed. Passed all tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -9,12 +9,26 @@ end
 
 def sum(array)
 	sumup = 0
-	array.each {|i| sumup = sumup +i }
+	array.each {|i| sumup += i }
 	return sumup
 end
 
 def multiply(array)
 	multUp = 1
-	array.each {|i| multUp = multUp*i }
+	array.each {|i| multUp *= i }
 	return multUp
+end
+
+def power(number1,number2)
+	number1**number2
+end
+
+def factorial(number)
+	result = 1 # factorial of 0 and 1, by definition
+	if number > 1 
+		number.downto(2) do
+			result = number*factorial(number-1) 
+		end
+	end
+	return result
 end
