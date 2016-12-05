93793d7df2e6df30aebd4b3f5c917e3199833875
Joao Roberto Lima <jbetol@gmail.com>
2016-12-05 14:57:22 -0200

    Complete. All tests ok.

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,8 +1,8 @@
 #write your code here
-def ftoc(temp)
-		((temp-32.0)/1.8).round 	
+def ftoc(f_temperature)
+		((f_temperature-32.0)/1.8).round 	
 end
 
-def ctof(temp)
-	(temp*9.0/5.0 + 32.0)
+def ctof(c_temperature)
+	(c_temperature*9.0/5.0 + 32.0)
 end
