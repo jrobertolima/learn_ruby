9462281592646db770b4dd300b98c7a0432c3f82
Joao Roberto Lima <jbetol@gmail.com>
2016-12-05 17:35:08 -0200

    Passed all tests. Complete

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
 class Book
 # write your code here
+	attr_writer :title
+
+	def title
+		titleize(@title)
+	end	
+
+	def titleize(title)
+		littleWords = ['and','a','an','the','to','or', 'in', 'for', 'of'] 
+		titleLocal = @title.split
+		titleLocal.each do |x|
+			if littleWords.index(x)==nil or (titleLocal.index(x)==0)
+				x.capitalize!			
+			end #if
+		end	 #do |x|
+		return titleLocal.join(" ")
+	end
+	
 end
+
+
