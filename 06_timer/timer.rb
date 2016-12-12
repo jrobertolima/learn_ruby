a359a951c603b5d08a1e08a5c0f450cfa739672e
Joao Roberto Lima <jbetol@gmail.com>
2016-12-12 13:41:00 -0200

    All tests passed. Complete

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,18 @@
 class Timer
   #write your code here
+	attr_writer :seconds
+	def initialize
+		@seconds = 0
+	end
+	
+	def seconds
+		@seconds	
+	end
+	
+	def time_string
+		t = Time.at(@seconds).utc
+		t.strftime("%H:%M:%S")
+	end
+	
+	
 end
